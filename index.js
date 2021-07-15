388835681aed578da7a8ed5653415e17df979bd8
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-07-15 14:04:56 -0500

    created employee class

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const inquirer = require('inquirer');
-const fs = require('fs')
+const fs = require('fs'); 
+const Manager = require('./lib/manager');
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const path = require('path');
+
+const OUTPUT_DIR = path.resolve(__dirname, 'output')
+const outputPath = path.join(OUTPUT_DIR, "output")
+
+const render = ('./lib/htmlRenderer');
